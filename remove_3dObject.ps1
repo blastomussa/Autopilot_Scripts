3c1d2e4911a3edef44f25b45c5488bf7059d8f5d
blastomussa <55928203+blastomussa@users.noreply.github.com>
2021-03-05 23:22:16 -0500

    Add files via upload
    Remove 3D objects folder from My PC

---
+++ remove_3dObject.ps1
@@ -0,0 +1,6 @@
+#Remove 3D objects folder from File Explorer
+
+#reg delete <keyname> [{/v Valuename | /ve | /va}] [/f]
+
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A} " /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}" /f
