9fff598740c587872cdf1524bdf99eb541e842ed
blasto <55928203+blastomussa@users.noreply.github.com>
2022-02-23 12:04:09 -0500

    Update Get-HWID.ps1

--- Get-HWID.ps1
+++ Get-HWID.ps1
@@ -3,6 +3,7 @@
     New-Item -Type Directory -Path $hwid_path -Force
     Set-Location -Path $hwid_path
     Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
     Install-Script -Name Get-WindowsAutoPilotInfo -Force
     Get-WindowsAutoPilotInfo.ps1 -OutputFile AutoPilotHWID.csv
