0d4ab4772fb654e77e9b8fba6f60b07866d76060
blasto <55928203+blastomussa@users.noreply.github.com>
2022-02-22 20:16:12 -0500

    Add Get-HWID script
    Extract HWID from windows 10 devices

---
+++ Get-HWID.ps1
@@ -0,0 +1,11 @@
+﻿function Get-HWID {
+    $hwid_path = (Join-Path -Path $env:USERPROFILE -ChildPath "\Desktop\HWID")
+    New-Item -Type Directory -Path $hwid_path -Force
+    Set-Location -Path $hwid_path
+    Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force
+    Install-Script -Name Get-WindowsAutoPilotInfo -Force
+    Get-WindowsAutoPilotInfo.ps1 -OutputFile AutoPilotHWID.csv
+    Set-ExecutionPolicy -Scope Process -ExecutionPolicy restricted -Force
+}
+
+Get-HWID
